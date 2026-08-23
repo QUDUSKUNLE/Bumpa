ddf7260087c93442ee54f844f44a5dcc60a77ddf
QUDUSKUNLE <18707476+QUDUSKUNLE@users.noreply.github.com>
2026-08-23 15:01:37 +0100

    Define domain

--- core/domain/events.go
+++ core/domain/events.go
@@ -9,6 +9,7 @@ import (
 
 type Event struct {
 	ID          uuid.UUID       `json:"id"`
+	UserID      uuid.UUID       `json:"user_id"`
 	Type        string          `json:"type"`
 	OccurredAt  time.Time       `json:"occurred_at"`
 	AggregateID uuid.UUID       `json:"aggregate_id"`
